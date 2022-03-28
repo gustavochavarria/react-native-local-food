b6d3e08d67dfc0970533d4d45e42f91854ad2792
gchava92 <gchava92@gmail.com>
2022-03-28 14:34:43 -0600

    WIP: add address icon and modal address

--- src/components/Restaurants/AddRestaurantsForm.js
+++ src/components/Restaurants/AddRestaurantsForm.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, StyleSheet } from "react-native";
+import { useState } from "react";
 import { useFormik } from "formik";
 
 import { Button, Input } from "@react-native-elements/base";
+
 import UploadImage from "./UploadImage";
+import MapForm from "./MapForm";
+
+const getColorIconMap = (formik) => {
+  if (formik.errors.location) return "#ff0000";
+
+  if (formik.values.location) return "#00a680";
+
+  return "#c2c2c2";
+};
 
 export default function AddRestaurants() {
+  const [showMap, setShowMap] = useState(false);
+
+  const onOpenCloseMap = () => setShowMap((prevState) => !prevState);
+
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -27,6 +41,12 @@ export default function AddRestaurants() {
 
       <Input
         placeholder="Address"
+        rightIcon={{
+          type: "material-community",
+          name: "map-marker-radius",
+          color: getColorIconMap(formik),
+          onPress: onOpenCloseMap,
+        }}
         onChangeText={(txt) => formik.setFieldValue("address", txt)}
       />
 
@@ -36,6 +56,8 @@ export default function AddRestaurants() {
       />
 
       <UploadImage formik={formik} />
+
+      <MapForm show={showMap} close={onOpenCloseMap} formik={formik} />
 
       <Button title="Create Restaurant" onPress={formik.handleSubmit} />
     </View>
---
+++ src/components/Restaurants/MapForm.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Button } from "@react-native-elements/base";
+
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+
+import Modal from "../Modal";
+
+export default function MapForm(props) {
+  const { show, close, formik } = props;
+
+  const [location, setLocation] = useState({
+    latitude: 0.001,
+    longitude: 0.001,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        console.error("not granted: ", status);
+
+        return;
+      }
+
+      const loc = await Location.getCurrentPositionAsync();
+
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      });
+    })();
+  });
+
+  const saveLocation = () => {
+    formik.setFieldValue("location", location);
+    close();
+  };
+
+  return (
+    <Modal show={show} close={close}>
+      <View>
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={location}
+          showsUserLocation={true}
+          onRegionChange={(location) => setLocation(location)}
+        >
+          <MapView.Marker draggable coordinate={location} />
+        </MapView>
+
+        <View style={styles.mapActions}>
+          <Button
+            title="Guardar"
+            containerStyle={styles.btnMapContainerSave}
+            buttonStyle={styles.btnMapSave}
+            onPress={saveLocation}
+          />
+          <Button
+            title="Cerrar"
+            containerStyle={styles.btnMapContainerCancel}
+            buttonStyle={styles.btnMapCancel}
+            onPress={close}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: "100%",
+    height: 550,
+  },
+  mapActions: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  btnMapContainerSave: {
+    paddingRight: 5,
+    width: "50%",
+  },
+  btnMapSave: {
+    backgroundColor: "#00a680",
+  },
+  btnMapContainerCancel: {
+    paddingLeft: 5,
+    width: "50%",
+  },
+  btnMapCancel: {
+    backgroundColor: "#a60d0d",
+  },
+});
